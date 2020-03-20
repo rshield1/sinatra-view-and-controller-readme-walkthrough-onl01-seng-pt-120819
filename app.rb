bc59d623595390b8bad62044e52699afa9142d39
Robert Shields <robert.shieldsiv@gmail.com>
2020-03-20 04:48:26 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -14,11 +14,6 @@ class App < Sinatra::Base
 
  get '/friends' do
   @friends = ['Emily Wilding Davison', 'Harriet Tubman', 'Joan of Arc', 'Malala Yousafzai', 'Sojourner Truth']
-  
-  <% @friends.each do |friend| %>
-    <h2><%= friend %></h2>
-<% end %>
- 
   erb :friends
   end
 end
